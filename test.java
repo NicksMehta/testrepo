80b73e5a86e99fc3846081ed6e06cd13f200f6f2
18shubhamgupta <18shubhamgupta@gmail.com>
2015-08-31 16:52:37 +0530

    My second commit

---
+++ test.java
@@ -0,0 +1,22 @@
+/**
+shubs
+
+ */
+public class Test {
+	public String mobile;
+	public String cover
+	public Date date;
+	public String mobile() {
+		return this.mbile;
+	}
+	public void setMobile(String name) {
+
+		this.mobile = mobile
+
+		this.cover = cover;
+	}
+	
+	public Date getDate() {
+		return this.date;
+	}
+}
