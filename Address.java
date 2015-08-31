01e223e0ffd8112edad191ce608e9e312f568887
nmehta <nikhil.mehta@paxcel.net>
2015-08-31 16:52:52 +0530

    Added address bean.

---
+++ Address.java
@@ -0,0 +1,18 @@
+/**
+ *
+ * Address Bean.
+ * @author: Nik
+
+ */
+public class Address {
+	public String streetName;
+	
+	public String city;
+	
+	public String getCity() {
+		return this.city;
+	}
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
+	}
+}
