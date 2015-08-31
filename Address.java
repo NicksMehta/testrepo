1918bb233314a5c9ad8438216d68e75d0d96135e
nmehta <nikhil.mehta@paxcel.net>
2015-08-31 17:05:23 +0530

    Committing  my address changes.

--- Address.java
+++ Address.java
@@ -8,11 +8,16 @@ public class Address {
 	public String streetName;
 	
 	public String city;
+	public String country;
 	
 	public String getCity() {
 		return this.city;
 	}
 	public void setStreetName(String streetName) {
 		this.streetName = streetName;
+	}
+	
+	public String getCountry() {
+		return this.country;
 	}
 }
